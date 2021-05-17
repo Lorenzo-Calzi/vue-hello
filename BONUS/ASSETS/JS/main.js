16c3291e0e6b4c22c2bdfa3cc8f7c49b38697169
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-05-17 14:43:13 +0200

    Aggiunta immagine tramite data

---
+++ BONUS/ASSETS/JS/main.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: "#root",
+
+    data: {
+      introduction: 'Inserisci i tuoi dati',
+      message: 'Benvenuto',
+      nome: "",
+      cognome: "",
+      image: '../BONUS/IMG/avatar.png'
+    }
+})
